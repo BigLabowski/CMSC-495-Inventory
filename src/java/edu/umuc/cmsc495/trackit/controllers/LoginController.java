9c2ba9fc6ef87c489a5b67ef7730b5460419cf88
Jacob <jpoage03@hotmail.com>
2015-04-26 14:08:43 -0700

    Revert "Sunday #1 (4-26-15)"

--- src/java/edu/umuc/cmsc495/trackit/controllers/LoginController.java
+++ src/java/edu/umuc/cmsc495/trackit/controllers/LoginController.java
@@ -1,19 +1,13 @@
 package edu.umuc.cmsc495.trackit.controllers;
 
-import edu.umuc.cmsc495.exceptions.InvalidLoginException;
-import edu.umuc.cmsc495.exceptions.LockedAccountException;
 import edu.umuc.cmsc495.trackit.models.DatastoreSingleton;
 import edu.umuc.cmsc495.trackit.models.Login;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 /**
  * Represents the login view
@@ -24,36 +18,23 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class LoginController {
     
     @RequestMapping(method = RequestMethod.GET)
-    public String login(ModelMap map, HttpSession session) {
-        session.setAttribute("user-login", null);
+    public String login(ModelMap map) {
         // Must return name of file (minus .jsp) under /views
         return "login";
     }
     
     @RequestMapping(method = RequestMethod.POST)
     public String login(@RequestParam("username") String username, 
-            @RequestParam("password") String password, ModelMap map,
-            HttpSession session) {
+            @RequestParam("password") String password, ModelMap map) {
         
         // Finding user by their credentials
-        Login login;
-        try {
-            login = DatastoreSingleton.getLoginByCredentials(username, password);
-        } catch (LockedAccountException ex) {
-            map.addAttribute("msg", "This account is locked, please try again later!");
-            return "login";
-        } catch (InvalidLoginException ex) {
-            map.addAttribute("msg", "Incorrect username or password, please try again!");
-            return "login";
-        }
+        Login login = DatastoreSingleton.getLoginByCredentials(username, password);
         boolean authenticated = (login != null);
         
         if (authenticated) {
             // Authenticated gets redirected to the home page
             map.addAttribute("from", "login");
             map.addAttribute("msg", "Thanks for signing in!");
-            // Adding user's session
-            session.setAttribute("user-login", login);
             return "redirect:home";
         } else {
             // No authentication exists, refreshing page with error
